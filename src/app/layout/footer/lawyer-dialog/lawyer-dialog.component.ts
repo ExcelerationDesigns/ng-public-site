cc40265adfabeab17ad1240e698dc0bf1ddfec93
JT Bruch <jtbruch103@gmail.com>
2017-01-28 20:24:59 -0700

    Updates home page (#9)
    * NS: Add color to the site & lawyer'y dialogs.

---
+++ src/app/layout/footer/lawyer-dialog/lawyer-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MdDialogRef } from '@angular/material';
+
+@Component({
+  selector: 'app-lawyer-dialog',
+  styleUrls: ['./lawyer-dialog.component.scss'],
+  templateUrl: './lawyer-dialog.component.html'
+})
+export class LawyerDialogComponent implements OnInit {
+
+  public textContent: string = '';
+
+  constructor(public dialogRef: MdDialogRef<LawyerDialogComponent>) { }
+
+  ngOnInit() {
+  }
+
+}
