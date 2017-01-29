bcaa7926d530a037b33be0facdebe9fb3cb7e658
JT-Bruch <jt.bruch@crownpeak.com>
2017-01-28 20:17:20 -0700

    NS: Address unit tests.

--- src/app/layout/footer/lawyer-dialog/lawyer-dialog.component.spec.ts
+++ src/app/layout/footer/lawyer-dialog/lawyer-dialog.component.spec.ts
@@ -1,25 +1,49 @@
 /* tslint:disable:no-unused-variable */
+import { NgModule } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MdDialogModule, MdDialog, OverlayContainer, MaterialModule } from '@angular/material';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { LawyerDialogComponent } from './lawyer-dialog.component';
 
+@NgModule({
+  declarations: [LawyerDialogComponent],
+  entryComponents: [LawyerDialogComponent],
+  exports: [LawyerDialogComponent],
+  imports: [MdDialogModule],
+})
+class DialogTestModule { }
+
+
 describe('LawyerDialogComponent', () => {
   let component: LawyerDialogComponent;
-  let fixture: ComponentFixture<LawyerDialogComponent>;
+  let dialog: MdDialog;
+  let overlayContainerElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LawyerDialogComponent ]
+      imports: [
+        DialogTestModule,
+        MdDialogModule.forRoot()
+      ],
+      providers: [
+        {
+          provide: OverlayContainer, useFactory: () => {
+            overlayContainerElement = document.createElement('div');
+            return { getContainerElement: () => overlayContainerElement };
+          }
+        }
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LawyerDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    dialog = TestBed.get(MdDialog);
+    const dialogRef = dialog.open(LawyerDialogComponent);
+
+    component = dialogRef.componentInstance;
   });
 
   it('should create', () => {
