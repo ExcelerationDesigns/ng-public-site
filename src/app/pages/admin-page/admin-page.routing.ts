ef649d0a20d3f71f569a41ada0d19cab3cf20093
JT-Bruch <jt.bruch@crownpeak.com>
2017-01-29 12:37:20 -0700

    NS: Adds components for admin section.

--- src/app/pages/admin-page/admin-page.routing.ts
+++ src/app/pages/admin-page/admin-page.routing.ts
@@ -1,3 +1,7 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { AuthGuardService } from './../../core/auth/auth-guard.service';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -5,9 +9,22 @@ import { AdminPageComponent } from './admin-page.component';
 
 const adminPageRoutes: Routes = [
   {
+    canActivate: [AuthGuardService],
+    children: [
+      {
+        canActivateChild: [AuthGuardService],
+        children: [
+          { component: BlogsComponent, path: 'blogs' },
+          { component: DetailsComponent, path: 'details' },
+          { component: DashboardComponent, path: '' }
+        ],
+        path: '',
+      }
+    ],
     component: AdminPageComponent,
-    path: ''
+    path: '',
   }
+
 ];
 
 @NgModule({
