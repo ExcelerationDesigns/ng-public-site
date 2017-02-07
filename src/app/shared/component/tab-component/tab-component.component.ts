2af5feb59b33cb10cd3852935b589cb00e8b5ea1
JT Bruch <jtbruch103@gmail.com>
2017-02-06 19:46:36 -0700

    NS: Makes tab component dynamic and loaded from lang file.

--- src/app/shared/component/tab-component/tab-component.component.ts
+++ src/app/shared/component/tab-component/tab-component.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+export interface TabContent {
+  content: string;
+  icon: string;
+  label: string;
+}
 
 @Component({
-  // encapsulation: ViewEncapsulation.None,
   selector: 'app-tab-component',
   styleUrls: ['./tab-component.component.scss'],
   templateUrl: './tab-component.component.html'
 })
 export class TabComponentComponent implements OnInit {
 
+  @Input() tabContent: TabContent[];
   constructor() { }
 
   ngOnInit() {
